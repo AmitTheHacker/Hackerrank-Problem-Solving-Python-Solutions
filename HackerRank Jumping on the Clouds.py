eeee52208186dc5b32d4edefa68d38b7b1947bd5
THE_DESTROYER__2020 <55714630+AmitTheHacker@users.noreply.github.com>
2020-06-07 17:52:39 +0530

    Add files via upload

---
+++ HackerRank Jumping on the Clouds.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+c = [int(c_temp) for c_temp in input().strip().split(' ')]
+
+res = 0
+i = 0
+while i < n-1:
+    if i+2<n and c[i+2] == 0:
+        i = i+2
+        res += 1
+    else:
+        i = i+1
+        res += 1
+print(
